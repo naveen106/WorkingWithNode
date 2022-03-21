0a7a84eef58041bfe099ee7285c5b613a5ad9c13
Naveen <nk0187912@gmail.com>
2022-03-21 16:01:27 +0530

    Models (data). Using MVC software design pattern.

---
+++ ExpressProject/Models/friends.model.js
@@ -0,0 +1,16 @@
+const friends = [
+   {
+      id: 0,
+      name: `Sir Isaac Newton`
+   },
+   {
+      id: 1,
+      name: `Alfred Nobel`
+   },
+   {
+      id: 2,
+      name: `Albert Einstein`
+   }
+];
+
+module.exports = friends;
