cb98639e173d54a3221ba79c06fadb94b38a394c
Naveen <nk0187912@gmail.com>
2022-03-21 15:20:14 +0530

    added some functions for messages controlling

---
+++ ExpressProject/Controller/messages.controller.js
@@ -0,0 +1,7 @@
+function getMessages(req, response){
+   response.send("Poor me, no one has spoken to me yet, no messages to show.");
+}
+
+function postMessages(req, res) { 
+   console.log(`Updatig data....`); 
+}
