8db58955a7cd0a76d523426b191fed35a4a91166
Naveen <nk0187912@gmail.com>
2022-03-21 16:02:47 +0530

    friends data (database consisting of friends.)

--- ExpressProject/Models/friends.model.js
+++ ExpressProject/Models/friends.model.js
@@ -3,10 +3,12 @@ const friends = [
       id: 0,
       name: `Sir Isaac Newton`
    },
+
    {
       id: 1,
       name: `Alfred Nobel`
    },
+   
    {
       id: 2,
       name: `Albert Einstein`
