389554591b2484b280fbf49bafa041996864a3e0
Naveen <nk0187912@gmail.com>
2022-03-21 16:53:12 +0530

    main server

--- ExpressProject/server.js
+++ ExpressProject/server.js
@@ -1,21 +1,14 @@
-
 //// PLEASE TAKE REFERENCE FROM 'index.js' file in **HTTP-SERVER** folder,
 //// see and compare how easy to follow and clean code is written with EXPRESS framework.
 
-
 const express = require('express');
-//const res = require('express/lib/response');
+const friendsRouter = require('./Routers/friends.router');
+const messagesRouter = require('./Routers/messages.router');
 const app = express();
-
-const messageController = require('./Controller/messages.controller');
-const friendsController = require('./Controller/friends.controller');
-
-
 const PORT = 3000;
 
+/////////////////// MIDDLEWARES  ////////////////////
 
-
-/////////////////// MIDDLEWARES
 //this runs and after next() executes, it goes to next Middleware(if there is),
 //otherwise it means it has reached the ENDPOINT. It runs the corresponding method(GET,POST etc) with given url.
 //meaning sends url according our defined way and sends the response.
@@ -28,24 +21,15 @@ app.use((req, res, next) => {    //.use() function to create middleware.
    console.log(`Method:${req.method}, Url: ${req.url}`);   
    console.log(`Time taken: ${(Date.now()-start)}ms\n`);
 });
- 
 
 // middleware to parse the request data provided as json object for post method.
 app.use(express.json());
 
+///// URL's are handled by ROUTER
+///// CONTROLLER  handles RESPONSES for REQUESTS (manipulates database according to request's need).
+///// MODELS has database.
 
-
-
-///////////////////SEND RESPONSE FOR GET REQUEST 
-app.get(`/friends`, friendsController.getAllFriends);
-app.get(`/friends/:friendId`, friendsController.getSingleFriend);
-app.get(`/messages`, messageController.getMessages);
-
-
-///////////////// SEND RESPONSE FOR POST REQUEST
-app.post(`/friends`, friendsController.postFriend); //add a friend to friends/array (simply our little database).
-app.post(`/messages`, messageController.postMessages); //add a message to 'messages' (well we don't have any messages).
-
-
+app.use('/messages', messagesRouter);
+app.use('/friends', friendsRouter);
 
 app.listen(PORT, () => { console.log(`Listening on ${PORT}...`) });
