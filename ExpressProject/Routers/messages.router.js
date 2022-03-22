f493c78256287f6735eba7939afa0dedf246cfc2
Naveen <nk0187912@gmail.com>
2022-03-22 15:00:06 +0530

    send appropriate response as per request

--- ExpressProject/Routers/messages.router.js
+++ ExpressProject/Routers/messages.router.js
@@ -2,9 +2,6 @@ const express = require('express');
 const messageController = require('../Controller/messages.controller');
 const messagesRouter = express.Router();
 
-// messagesRouter.use('/messages', (req, res, next) => {
-//    next();
-// });
 messagesRouter.get(`/`, messageController.getMessages);
 messagesRouter.post(`/`, messageController.postMessages); //add a message to 'messages' (well we don't have any messages).
 
