749da4d196c540af24796c93a78de498ee770baf
Naveen <nk0187912@gmail.com>
2022-03-22 14:58:42 +0530

    message Controller, send appropriate response to message url

--- ExpressProject/Controller/messages.controller.js
+++ ExpressProject/Controller/messages.controller.js
@@ -1,5 +1,16 @@
-function getMessages(req, response){
-   response.send("Poor me, no one has spoken to me yet, no messages to show.");
+const path = require('path'); //node built-in module
+
+function getMessages(req, res) {
+   //res.send("Poor me, no one has spoken to me yet, no messages to show.");
+
+   //Notice that the function used to send File is "sendFile()" instead of 'send()'.
+   //__dirname is built-in variable containing name of source file that is being executed.(and that is server.js).
+   
+   //res.sendFile(path.join(__dirname, '..', 'public', 'yourLieInApril.jpg'));  //join function will join and make the path from provided arguments.
+
+   res.render('messages', {
+      "friend": 'Elon Musk'
+   })
 }
 
 function postMessages(req, res) { 
@@ -8,6 +19,6 @@ function postMessages(req, res) {
 }
 
 module.exports = {
-   getMessages,
-   postMessages
+  getMessages,
+  postMessages,
 };
